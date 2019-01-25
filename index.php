f568056172d9f025f2493322627b402baa805bca
Alec <avan-etten@mail.greenriver.edu>
2019-01-25 02:03:51 -0500

    fixed routes, added results route

--- index.php
+++ index.php
@@ -21,10 +21,18 @@ $f3->route('GET /order', function() {
     echo $view->render('views/form1.html');
 });
 
+$f3->route('POST /order2', function() {
+    $view = new View;
+    echo $view->render('views/form2.html');
+});
 
+$f3->route('POST /results', function() {
+    $view = new View;
+    echo $view->render('views/results.html');
+});
 
 //Define a route that accepts a parameter for animal type
-$f3->route('GET /@animal', function($f3, $params)
+$f3->route('POST /@animal', function($f3, $params)
 {
     switch ($params['animal'])
     {
